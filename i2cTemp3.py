853edff9b225190977b53a5c88df850f3e6bb789
Rolf Eriksen <rwe@netcom.no>
2015-03-09 22:24:40 +0100

    i2c Temp 12 bit fix

--- i2cTemp3.py
+++ i2cTemp3.py
@@ -12,21 +12,21 @@ def readTemp9b():
 
 def readTemp12b():
         tmp1 = bus.read_word_data(address, 0)
-        print "12b", tmp1
-        tmp1 = (0xff & tmp1 << 4) | (tmp1 >> 12)
-        return tmp1 / 16.0
+        print "12b", tmp1, ", Bin: ", bin(0xff00 & tmp1), bin(0xff & tmp1) 
+        #tmp1 = (c << 4) | (tmp1 >> 12)
+        return (0xff & tmp1) + (tmp1 >> 12) / 16.0
 
 def tempSet12b():
         print "c:", bus.read_byte_data(address, 1)
         bus.write_byte_data(address, 0x01, 0x60)
         print "c:", bus.read_byte_data(address, 1)
 
-for i in range(0, 2):
+for i in range(0, 6):
         print "Temp:", i, readTemp9b()
         time.sleep(1)
 
 
 tempSet12b()
-for i in range(0, 2):
+for i in range(0, 60):
         print "Temp:", i, readTemp12b()
         time.sleep(1)
