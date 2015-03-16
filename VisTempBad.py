92f57ba29cd42404a71242783ccbf38003c7fe1a
rwenor <rwe@netcom.no>
2015-03-16 08:28:37 +0100

    VisBad-Args

--- VisTempBad.py
+++ VisTempBad.py
@@ -2,6 +2,26 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pylab as pl
+import sys, getopt
+
+
+try:
+        opts, args = getopt.getopt(sys.argv[1:], 's', ['tull=', 's='])
+except getopt.GetoptError as err:
+        print str(err)
+        print "Feil args"
+        #usage()
+        sys.exit(2)
+        
+print opts
+print args
+saveFile = ''
+for o, a in opts:
+    if o == '-s':
+        saveFile = args[0]
+    elif o == '--s':
+        saveFile = a
+        
 
 data1 = []
 data2 = []
@@ -31,7 +51,7 @@ with open('TempLogBad.dat', 'r') as f:
                 data3.append( float( l[2] ) / 10.0 + 20)
                 
 #print data
-
+#pl.figure(1)
 pl.plot(data1)
 pl.plot(data2)
 pl.plot(data3)
@@ -40,6 +60,11 @@ pl.xlabel('x')
 pl.ylabel('Temp / hum/10')
 # pl.xlim(0.0, 10.)
 pl.grid(color='r')
-pl.show()
+
+if saveFile <> '':
+    pl.savefig(saveFile)   
+else:
+    pl.show()
+
 
 #data=np.genfromtxt('test.csv', dtype='i4,i2,i2,S3,S3,S3', names='Y,M,D,A,B,C')
