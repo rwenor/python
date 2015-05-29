3e6414c634a0b225b0b25241c78e717934304556
rwenor <rwe@netcom.no>
2015-05-29 18:45:48 +0200

    img_pool.py

--- img_client_pi.py
+++ img_client_pi.py
@@ -7,7 +7,8 @@ import picamera
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 client_socket = socket.socket()
-client_socket.connect(('192.168.1.201', 8000))
+client_socket.connect(('0.0.0.0', 8000))
+#client_socket.connect(('192.168.1.201', 8000))
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
@@ -24,17 +25,18 @@ try:
         # our protocol simple)
         start = time.time()
         stream = io.BytesIO()
+        i = 0
         for foo in camera.capture_continuous(stream, 'bmp'):
             # Write the length of the capture to the stream and flush to
             # ensure it actually gets sent
 
             print stream.tell()
             
-            connection.write(struct.pack('>L', stream.tell()))
-            connection.flush()
+#            connection.write(struct.pack('>L', stream.tell()))
+#            connection.flush()
             # Rewind the stream and send the image data over the wire
             stream.seek(0)
-            connection.write(stream.read())
+#            connection.write(stream.read())
 
             print 'Done'
             
@@ -44,8 +46,13 @@ try:
             # Reset the stream for the next capture
             stream.seek(0)
             stream.truncate()
+
+            i += 1
+
+            
     # Write a length of zero to the stream to signal we're done
     connection.write(struct.pack('<L', 0))
+    print i
 finally:
     connection.close()
     client_socket.close()
--- test_image_cv.py
+++ test_image_cv.py
@@ -19,17 +19,20 @@ rawCapture = PiRGBArray(camera)
 # allow the camera to warmup
 time.sleep(0.1)
 
-for i in range(2):  #virket ikke
+for i in range(200):  #virket ikke
     # grab an image from the camera
     camera.capture(rawCapture, format="bgr")
     image = rawCapture.array
-     
+
     # display the image on screen and wait for a keypress
     cv2.imshow("Image", image)
-    #rawCapture = PiRGBArray(camera)
+    
 
     p('i: '+ str(i))
-    cv2.waitKey(0)
+    cv2.waitKey(1)
+    p('o: '+ str(i))
+    rawCapture = PiRGBArray(camera)
+    time.sleep(0)
     
 print 'wk'
 cv2.waitKey(0)
