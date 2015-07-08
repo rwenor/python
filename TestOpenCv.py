7248e879ad3895ed230c937c21fb1db3731a9d9a
rwenor <rwe@netcom.no>
2015-07-08 19:26:48 +0200

    %1

--- TestOpenCv.py
+++ TestOpenCv.py
@@ -4,4 +4,10 @@ cap = cv2.VideoCapture(0)
 while True:
     b,frame = cap.read()
     cv2.imshow("frame", frame)
-    cv2.waitKey(1)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break   
+
+
+cap.release()
+cv2.destroyAllWindows()
