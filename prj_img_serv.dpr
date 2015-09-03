ce01864a36cd283d50d269fd8bbaf4f127b3bb76
unknown <rolf@ROLF-WIN7.AXICON.local>
2015-09-03 09:28:46 +0200

    %1

---
+++ prj_img_serv.dpr
@@ -0,0 +1,14 @@
+program prj_img_serv;
+
+uses
+  Vcl.Forms,
+  Img_serv in 'Img_serv.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
