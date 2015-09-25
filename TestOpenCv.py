903549534469ec7f4204f249bd48a2597514035b
rwenor <rwe@netcom.no>
2015-09-25 09:02:03 +0200

    %1

--- TestOpenCv.py
+++ TestOpenCv.py
@@ -1,13 +1,27 @@
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
+i = 0
+b,frame = cap.read()  # Les forste bilde
+
+print "Start"
+t0 = time.time()
 while True:
     b,frame = cap.read()
     cv2.imshow("frame", frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break   
+    i += 1
+    #print i
+    t = time.time() - t0
+    #print t
+    
+    if t >  30:
+        break
 
+print "Bilder i sekundet: ", i / t
 
 cap.release()
 cv2.destroyAllWindows()
