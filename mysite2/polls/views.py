766b60d71713df0e72ca05bc9e95b1111ddd62fe
Rolf Eriksen <rwe@netcom.no>
2015-10-23 08:37:28 +0200

    %1

--- mysite2/polls/views.py
+++ mysite2/polls/views.py
@@ -6,6 +6,8 @@ from django.utils import timezone
 
 from .models import Question, Ma_Part, Mn_Name
 from .models import Choice
+import pandas as pd
+import math
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -78,32 +80,64 @@ from django.http import HttpResponse
 
 
 def listParts():
-    parts = Ma_Part.objects.order_by('ma_desc')
+    parts = Ma_Part.objects.all() #.order_by('ma_desc')
 
     output = ''
+    i = 0
     for p in parts:
-        #print p.mn_id
+        print i, p
+        print str(p.ma_id),'|', p.ma_desc,'|', p.mn_id.mn_name,'|', p.mn_nr
 
-        output += '<p>{0}: {1}  - {2}: {3}</p>'.format(str(p.ma_id), p.ma_desc, p.mn_id.mn_name, p.mn_nr)
+        try:
+            output += '<p>{0}: {1}  - {2}: {3}</p>'.format(str(p.ma_id), unicode(p.ma_desc), p.mn_id.mn_name, p.mn_nr)
+        except:
+            print "*************"
+            pass
+        print i, p.ma_id, p.ma_desc
+        i += 1
     return output
 
+
 def indexHello(request):
-    parts = Ma_Part.objects.order_by('ma_desc')
+    output = listParts()
+    return HttpResponse( output )
 
+
+def listDf(df):
     output = ''
-    for p in parts:
+    for i in df.index:
+        if math.isnan( df.iloc[i]['mamut nr'] ):
+            print 'Next', i
+            continue
+            
+            
+        output += '<p>%d\t%s\t%s</p>' % (i, df.iloc[i][6], df.iloc[i]['levr pro nr'],) 
+        mn, c = Mn_Name.objects.get_or_create(mn_name=df.iloc[i][6]) 
+        mn.save()  
+        
+        output += '<p>%d\t%d\t%s</p>' % (i, df.iloc[i]['mamut nr'], df.iloc[i]['beskrivelse'],)
+        p = Ma_Part.objects.get_or_create(ma_id=df.iloc[i]['mamut nr'], ma_desc=df.iloc[i]['beskrivelse'], mn_id=mn, mn_nr=df.iloc[i]['levr pro nr'], mn_desc='')
+        print p
+        
+        try: 
+            p.save()
+        except:
+            pass
+        print i
+        
+        
+        
+        #output += '<p>%d\t%d\t%s</p>' % (i, df.iloc[i][6], df.iloc[i]['levr pro nr'],)
         #print p.mn_id
-
-        output += '<p>{0}: {1}  - {2}: {3}</p>'.format(str(p.ma_id), p.ma_desc, p.mn_id.mn_name, p.mn_nr)
-
-
-    ax, c = Mn_Name.objects.get_or_create(mn_name='Axicon')
-    axp = Ma_Part.objects.create(ma_id=102, ma_desc='Ax 2', mn_id=ax, mn_nr='1010101', mn_desc='foo ooo o o')
-    axp.save()
-
-
-    output += listParts()
-
-
+        #output += '<p>{0}: {1}  - {2}: {3}</p>'.format(str(p.ma_id), p.ma_desc, p.mn_id.mn_name, p.mn_nr)
+    return output
+    
+    
+def indexHello2(request):
+    import pandas as pd
+    df = pd.read_csv("parts-ax200", sep='\t')
+    
+    output = listDf(df) 
+    #$listParts()
     return HttpResponse( output )
 
