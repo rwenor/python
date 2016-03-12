9fac903141c8352594908fd8ed977b4fc5388489
Rolf MBA-Mint <rwenor1@icloud.com>
2016-03-12 11:59:11 +0100

    %1

--- chat-example/index.js
+++ chat-example/index.js
@@ -2,14 +2,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var messages = [];
+    sockets = [];
+
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.broadcast.emit('chat display', 'a user connected');
+  socket.emit('chat all', messages);
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
@@ -17,12 +21,13 @@ io.on('connection', function(socket){
   });
 
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    socket.broadcast.emit('chat message', msg);
+    console.log('message: ' + msg.msg);
+    messages.push(msg);
+    io.emit('chat message', msg);
   });
 
   socket.on('chat typer', function(msg){
-    console.log('message: ' + msg);
+    console.log('Typer: ' + JSON.stringify(msg));
     socket.broadcast.emit('chat typer', msg);
   });
 
